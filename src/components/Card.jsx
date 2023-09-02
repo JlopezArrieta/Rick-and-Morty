3ab9f2db6cfeacfcee33971985de030f44ffe44e
JlopezArrieta <javilop87@hotmail.com>
2023-09-02 14:49:38 -0500

    RyM React-Intro

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,15 @@
-export default function Card(props) {
+
+export default function Card({id, name, status, species, gender, origin, image, onClose}) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={onClose}>X</button>
+         <h2 id='id'>{id}</h2>
+         <h2 id='name'>{name}</h2>
+         <h2 id='status'>{status}</h2>
+         <h2 id='species'>{species}</h2>
+         <h2 id='gender'>{gender}</h2>
+         <h2 id='origin'>{origin.name}</h2>
+         <img src={image} alt='name' />
       </div>
    );
 }
