fd20ad0fba187684154b2dd71a60596c0d425c9e
JlopezArrieta <javilop87@hotmail.com>
2023-09-20 00:40:12 -0500

    web server clase m3

---
+++ Client/src/App.jsx
@@ -0,0 +1,84 @@
+import "./App.css";
+import Cards from "./components/Cards/Cards.jsx";
+import Nav from "./components/Nav/Nav.jsx";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import About from "./components/About/About";
+import Detail from "./components/Detail/Detail";
+import Error from "./components/Error/Error";
+import Form from "./components/Form/Form";
+import Favorites from "./components/Favorites/Favorites";
+// import validation from "./components/Form/validation";
+
+
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+
+  // const location = useLocation()
+  const { pathname } = useLocation()
+  // console.log(location)
+
+  const [access, setAccess] = useState(false);
+
+  const EMAIL = 'jalop123@gmail.com';
+  const PASSWORD = 'javier7';
+
+  const navigate = useNavigate();
+
+  //userData por destructuri = {email, password}
+  function login({ email, password }) {
+    if (email === EMAIL && password === PASSWORD) {
+      setAccess(true);
+      navigate('/home');
+    }
+    else alert('Usuario o Contraseña Incorrecta')
+  }
+
+  const onSearch = (id) => {
+    if (!id || isNaN(id)) return alert('Ingrese Un Id Valido');
+    if (characters.some(chart => chart.id === Number(id))) return alert('El Id de este personaje ya esta ingresado');
+
+    axios(`http://localhost:3001/rickandmorty/character/${id}`)
+      .then((response) => response.data)
+      .then((data) => {
+        if (data.name) {
+          setCharacters((oldChars) => [...oldChars, data]);
+        } else {
+          window.alert('¡Ingrese un ID!');
+        }
+      })
+      .catch((error) => alert("No existe un personaje con este ID"));
+  }
+
+
+  const onClose = (id) => {
+    const charactersFilter = characters.filter((character) => character.id !== Number(id))
+    setCharacters(charactersFilter)
+  }
+
+  useEffect(() => {
+    !access && navigate('/');
+  }, [access]);
+
+  const handleLogOut = () => {
+    setAccess(false);
+  }
+
+  return (
+    <div className="App" >
+      {pathname !== '/' && <Nav onSearch={onSearch} handleLogOut={handleLogOut} />}
+      <Routes>
+        <Route path="/" element={<Form login={login} />} />
+        <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/detail/:id" element={<Detail />} />
+        <Route path="*" element={<Error />} />
+        <Route path='/Favorites' element={<Favorites />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Client/src/components/Detail/Detail.jsx
@@ -0,0 +1,39 @@
+import style from './Detail.module.css';
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+
+const Detail = () => {
+  const { id } = useParams();
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios(`http://localhost:3001/rickandmorty/character/${id}`)
+      .then(response => response.data)
+      .then((data) => {
+        if (data.name) {
+          setCharacter(data);
+        } 
+        else {
+          window.alert('No hay personajes con ese ID');
+        }
+      });
+    return setCharacter({});
+  }, [id]);
+
+  return (
+    <div className={style.container}>
+      <h2 className={style.num}>{character?.id}</h2>
+      <h1 className={style.estiloLetrasRick}>{character?.name}</h1>
+      <div className={style.estiloLetras}>
+        <h2>{character?.status}</h2>
+        <h2>{character?.species}</h2>
+        <h2>{character?.gender}</h2>
+        <h2>{character?.origin?.name}</h2>
+      </div>
+      <img className={style.imagenRick} src={character?.image} alt={character?.name} />
+    </div>
+  )
+}
+
+export default Detail;
